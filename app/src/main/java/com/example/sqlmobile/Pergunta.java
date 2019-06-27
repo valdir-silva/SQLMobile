ab09a80f71d5da48e2ac8587578b0f3852bc5779
Valdir Silva <whiteofdark@outlook.com>
2019-06-27 10:07:46 -0300

    Start Commit!

---
+++ app/src/main/java/com/example/sqlmobile/Pergunta.java
@@ -0,0 +1,50 @@
+package com.example.sqlmobile;
+
+public class Pergunta {
+
+    private int id;
+    private String pergunta;
+    private String resposta1;
+    private String resposta2; //alterar isso por um array
+    private int respostaCerta;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    public void setPergunta(String pergunta) {
+        this.pergunta = pergunta;
+    }
+
+    public String getResposta1() {
+        return resposta1;
+    }
+
+    public void setResposta1(String resposta1) {
+        this.resposta1 = resposta1;
+    }
+
+    public String getResposta2() {
+        return resposta2;
+    }
+
+    public void setResposta2(String resposta2) {
+        this.resposta2 = resposta2;
+    }
+
+    public int getRespostaCerta() {
+        return respostaCerta;
+    }
+
+    public void setRespostaCerta(int respostaCerta) {
+        this.respostaCerta = respostaCerta;
+    }
+}
